a3a71c507eaf2fb0d1569f5ddb483eac07a6af7d
Sandro-Col <sandro.col@gmail.com>
2024-02-03 18:19:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
+import ResultContainerPlugin from './ResultContainerPlugin.jsx';
+
 function App() {
-  return (
+
+    const [decodedResults, setDecodedResults] = useState([]);
+    const onNewScanResult = (decodedText, decodedResult) => {
+        console.log("App [result]", decodedResult);
+        setDecodedResults(prev => [...prev, decodedResult]);
+    };
+
+  /* return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Oi.
         </p>
         <a
           className="App-link"
@@ -19,7 +31,66 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  ); */
+
+
+  return (
+    <div className="App">
+        <section className="App-section">
+            <div className="App-section-title"> Sandro - Html5-qrcode React demo</div>
+            <br />
+            <br />
+            <br />
+            <Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
+            <ResultContainerPlugin results={decodedResults} />
+        </section>
+    </div>
+);
+
 }
 
 export default App;
+
+
+// // @ts-check
+
+// import React, { useState } from 'react';
+// import './App.css';
+// // import HowToUse from './HowToUse.jsx';
+// import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
+// import ResultContainerPlugin from './ResultContainerPlugin.jsx';
+
+// const App = (props) => {
+//     const [decodedResults, setDecodedResults] = useState([]);
+//     const onNewScanResult = (decodedText, decodedResult) => {
+//         console.log("App [result]", decodedResult);
+//         setDecodedResults(prev => [...prev, decodedResult]);
+//     };
+
+//     return (
+//         <div className="App">
+//             <section className="App-section">
+//                 <div className="App-section-title"> Html5-qrcode React demo</div>
+//                 <br />
+//                 <br />
+//                 <br />
+//                 <Html5QrcodePlugin
+//                     fps={10}
+//                     qrbox={250}
+//                     disableFlip={false}
+//                     qrCodeSuccessCallback={onNewScanResult}
+//                 />
+//                 <ResultContainerPlugin results={decodedResults} />
+                
+//             </section>
+//         </div>
+//     );
+// };
+
+// export default App;
+
